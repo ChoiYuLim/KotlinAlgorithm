20b28fa1983b76b3de7d8998a33554ab559be246
yulim <yulim13sky@naver.com>
2022-11-04 16:22:50 +0900

    refactor: 이코테 Greedy 3번 문제

--- src/main/kotlin/이코테Greedy3.kt
+++ src/main/kotlin/이코테Greedy3.kt
@@ -1,10 +1,10 @@
-fun main(){
-    val n = readln().map{ it.toString().toInt() }
+fun main() {
+    val n = readln().map { it.toString().toInt() }
     var answer = 0
-    n.forEach{
-        if(it==0 || it==1 ||answer==0 || answer==1){
-            answer+=it
-        }else answer *= it
+    n.forEach {
+        if (it <= 1 || answer <= 1) {
+            answer += it
+        } else answer *= it
     }
     print(answer)
 }
